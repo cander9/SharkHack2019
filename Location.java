2c7584c2a6806339b97ab595edd17f6eb269fc1e
cander9 <49759197+cander9@users.noreply.github.com>
2019-09-28 02:14:36 -0400

    Update Location.java

--- Location.java
+++ Location.java
@@ -1,17 +1,16 @@
 public class Location {
-    String loc;
+    String loc = " ";
     String race;
     double deathRisk;
     double compRisk;
     int homeCost;
     int insuredCost;
     int notInsuredCost;
+    Mom bob;
 
-    Location(String l,int hc,int ic,int nic){
+    Location(String l,Mom b){
         loc = l;
-        homeCost = hc;
-        insuredCost = ic;
-        notInsuredCost = nic;
+        bob = b;
     }
 
     public String getLoc() {
@@ -34,11 +33,64 @@ public class Location {
         return notInsuredCost;
     }
 
+    public void instantiateCosts() {
+        if (loc == "Boston") {
+            insuredCost = 3700;
+            notInsuredCost = 30000;
+            homeCost = 3100;
+        } else if (loc == "Atlanta") {
+            insuredCost = 6693;
+            notInsuredCost = 12447;
+            homeCost = 3500;
+        } else if (loc == "Buenos Aires") {
+            insuredCost = 106;
+            notInsuredCost = 2237;
+        } else if (loc == "Berlin") {
+            insuredCost = 0;
+            notInsuredCost = 0;
+            homeCost = 0;
+        } else if (loc == "Cairo") {
+            insuredCost = 100;
+        } else if (loc == "Jakarta") {
+            insuredCost = 1650;
+            notInsuredCost = 250; //check with Lauren
+            homeCost = 0;
+        }
+    }
+
+    public void compCosts() {
+        boolean c = bob.isComplicated();
+        if(c){
+            if (loc == "Boston") {
+                insuredCost = 5750;
+                notInsuredCost = 50000;
+                homeCost = 3100;
+            } else if (loc == "Atlanta") {
+                insuredCost = 9427;
+                notInsuredCost = 16711;
+                homeCost = 3500;
+            } else if (loc == "Buenos Aires") {
+                insuredCost = 526;
+                notInsuredCost = 2972;
+            } else if (loc == "Berlin") {
+                insuredCost = 0;
+                notInsuredCost = 0;
+                homeCost = 0;
+            } else if (loc == "Cairo") {
+                insuredCost = 500;
+         } else if (loc == "Jakarta") {
+                insuredCost = 2205;
+                notInsuredCost = 460; //check with Lauren
+                homeCost = 0;
+            }
+        }
+    }
+
     public double locRisk() {
         if(loc=="Jakarta"){
             deathRisk = 63;
         }
-        else if(loc=="Egypt"){
+        else if(loc=="Cairo"){
             deathRisk = 17;
         }
         else if(loc=="Buenos Aires"){
@@ -69,7 +121,7 @@ public class Location {
     }
 
     public double compRisk() {
-        if(loc=="Egypt"||loc=="Buenos Aires") {
+        if(loc=="Cairo"||loc=="Buenos Aires") {
             compRisk = 75;
         }
         else{
