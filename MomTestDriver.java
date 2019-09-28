78175aa12b1daf6f579605c2f39d5a10c7fa4e7e
cander9 <49759197+cander9@users.noreply.github.com>
2019-09-28 02:13:32 -0400

    Update MomTestDriver.java

--- MomTestDriver.java
+++ MomTestDriver.java
@@ -3,17 +3,19 @@ public class MomTestDriver {
         int i;
         for(i = 0;i < 10;i++){
 
-            Mom bob = new Mom(30000,"black",44);
-            bob.setLoc("Jakarta",4000,4000,4000);
+            Mom bob = new Mom(50000,"black",44);
+            bob.setLoc("Cairo",bob);
             System.out.println("initial Money: "+bob.setInitialMoney());
             bob.setInsurance();
             System.out.println("Insured? "+bob.isInsured());
-            bob.setHome(true);
+            //bob.setHome(true);
+            System.out.println("Money before birth: "+bob.getMoney());
             System.out.println("Complicated? "+bob.complication());
             System.out.println(bob.isComplicated());
+            System.out.println("Money after birth: "+bob.getMoney());
             System.out.println("Dead? "+bob.death());
             System.out.println(bob.isDeath());
-            
+
         }
     }
 }
